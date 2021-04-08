f5fed9edbfdc8af011fdf2a7e6486fc568654867
Shivam <shivam09052000@gmail.com>
2021-04-08 19:45:24 +0100

    app

---
+++ android/app/src/main/kotlin/com/example/proa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,215 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> myBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  return MyAppState()._showNotification(message);
+}
+
+Future<void> main() async {
+  await WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(myBackgroundHandler);
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  
+  String fcmToken = "Getting Firebase Token";
+
+  @override
+  Future<void> initState(){
+    /**
+     * Requesting Permission
+     */
+    requestingPermissionForIOS();
+
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
+
+    super.initState();
+
+    FirebaseMessaging.onMessage.listen((message) {
+      print(message);
+      if (message.data.isNotEmpty) MyAppState()._showNotification(message);
+    });
+
+    getTokenz();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter FCM Notification'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "FCM TOKEN:",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 15),
+            Text(
+              fcmToken,
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 15),
+            FloatingActionButton(
+              child: Icon(Icons.send),
+              onPressed:(){
+                sendAndRetrieveMessage();
+              }
+             ),
+             SizedBox(height: 15),
+             Text("Push to send notification")
+          ],
+        ),
+      ),
+    );
+  }
+
+  requestingPermissionForIOS() async {
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
+
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true, // Required to display a heads up notification
+      badge: true,
+      sound: true,
+    );
+  }
+
+  Future _showNotification(RemoteMessage message) async {
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      'This channel is used for important notifications.', // description
+      importance: Importance.max,
+    );
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    print(message.data);
+    Map<String, dynamic> data = message.data;
+    AndroidNotification android = message.notification?.android;
+    if (data != null) {
+      flutterLocalNotificationsPlugin.show(
+        0,
+        data['title'],
+        data['body'],
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            channel.description,
+            icon: android?.smallIcon,
+            // other properties...
+          ),
+          iOS: IOSNotificationDetails(presentAlert: true, presentSound: true),
+        ),
+        payload: 'Default_Sound',
+      );
+    }
+  }
+
+  getTokenz() async {
+    String token = await _firebaseMessaging.getToken();
+    setState(() {
+      fcmToken = token;
+      print(fcmToken);
+    });
+  }
+
+  Future selectNotification(String payload) async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+
+
+
+
+  Future<Map<String, dynamic>> sendAndRetrieveMessage() async {
+    
+  final String serverToken = '<enter server key>';
+    var url=Uri.parse('https://fcm.googleapis.com/fcm/send');
+    await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'key=$serverToken',
+      },
+      body: jsonEncode(
+      <String, dynamic>{
+        'notification': <String, dynamic>{
+          'body': 'this is a body',
+          'title': 'this is a title'
+        },
+        'priority': 'high',
+        'data': <String, dynamic>{
+          'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+          'id': '1',
+          'status': 'done'
+        },
+        'to': '<Enter device token>',
+      },
+      ),
+    );
+  }
+}
+
+
+
+
+
